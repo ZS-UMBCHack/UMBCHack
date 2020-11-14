7c71c5d67abb57f235f7b06b4fd7407e391f68cb
Sophie Roberts-Weigert <sophiarrw@gmail.com>
2020-11-14 10:12:47 -0500

    Button

--- test.py
+++ test.py
@@ -3,38 +3,71 @@ import cairo
 import math
 import numpy
 
+red = (255, 0, 0)
+black = (0, 0, 0)
+white = (255, 255, 255)
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock()
-    width = 400
-    height = 300
-    screen = pygame.display.set_mode((width, height))
-
-    red = (255, 0, 0)
-    black = (0, 0, 0)
-
     running = True
-    button = pygame.Rect(0, 30, 60, 60)
+    
+    font = pygame.font.SysFont("comicsansms", 20)
 
     while running:
         # main loop
+        screen = def_screen(400, 300)
+        clock = pygame.time.Clock()
+        
+        clock.tick(120)
         screen.fill(black)
-        clock.tick(60)
+        
+        button, text = draw_button(screen, 0, 30, "Hello, World", font)
 
-        # pygame.draw.rect(screen, red, button)
-        round_rect(screen, button, red)
         pygame.display.flip()
-        button = button.move(1, 0)
 
-        # check for quit
-        for event in pygame.event.get():
+        running = check_for_quit(button)
+ 
+def draw_button(surface, bx, by, text, font, bcolor=white, tcolor=black):
+    T = def_text(text, font, tcolor)
+    render_text(surface, T, -1000, -1000)
+    twidth = T.get_width()
+    theight = T.get_height()
+    bwidth = twidth + twidth / 4
+    bheight = theight + theight / 4
+    
+    B = pygame.Rect(bx, by, bwidth, bheight)
+    round_rect(surface, B, bcolor)
+    cx, cy = get_center(bx, by, bwidth, bheight)
+    
+    render_text(surface, T, cx, cy)
+    
+    return B, T
+    
+def get_center(x, y, width, height):
+    return (x + width // 2, y + height // 2)
+       
+def move_button(button, text, dx=1, dy=0):
+    return button.move(dx, dy)
+    #text = text.move(dx, dy)
+
+def render_text(surface, text, cx, cy):
+    return surface.blit(text, (cx - text.get_width() // 2, cy - text.get_height() // 2))
+
+def def_text(text, font, color):
+    return font.render(text, True, color)
+      
+def check_for_quit(button):
+    boolean = True
+    for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                boolean = False
             elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:  # left click
                 if button.collidepoint(pygame.mouse.get_pos()):
                     print("click!")
+    return boolean
 
+def def_screen(width, height):
+     return pygame.display.set_mode((width, height))
 
 def round_rect(surface, rect, color, radius=0.4):
     """
@@ -43,7 +76,7 @@ def round_rect(surface, rect, color, radius=0.4):
     color   : rgb or rgba
     radius  : 0 <= radius <= 1
     """
-
+    
     rect = pygame.Rect(rect)
     color = pygame.Color(*color)
     alpha = color.a
