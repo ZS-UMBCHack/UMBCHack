c864bfcfed1d12f386ead28d0b8959ac2169ea17
Sophia Roberts-Weigert <sophiarrw@gmail.com>
2020-11-15 01:28:24 -0500

    drag?

--- src/main.py
+++ src/main.py
@@ -1,8 +1,5 @@
-import pygame
-
-red = (255, 0, 0)
-black = (0, 0, 0)
-white = (255, 255, 255)
+import pygame, graphics
+from graphics import Box, BoxText, Text
 
 NA = 0
 
@@ -17,111 +14,110 @@ ROWSPACING = 25
 cx = SWIDTH / 2
 cy = SHEIGHT / 2
 
+yi = 20
 
 def main():
     pygame.init()
+    
+    screen = def_screen(SWIDTH, SHEIGHT)
+    screen.fill(graphics.black)
+    clock = pygame.time.Clock()
     running = True
+    screen_draging = False
+    obj = []
 
-    font = pygame.font.Font("assets/OpenDyslexic3-Regular.ttf", 15)
-
+    font = pygame.font.Font("assets/OpenDyslexic3-Regular.ttf", 20)
     view_pos = 0, 0
-
-    obj = tree([[{"Language": "Latin", "Word": "sonus"}, {"Language": "", "Word": ""}],
-                [{"Language": "Anglo-Norman French", "Word": "soun/suner"}, {"Language": "", "Word": ""}],
-                [{"Language": "Middle English", "Word": "soun"}, {"Language": "English", "Word": "-d"}],
-                [{"Language": "", "Word": "sound"}, {"Language": "", "Word": "sound"}]], 20, font)
-
+    
+    
+    y = yi
+    y, words = def_word("goodbye", font, yi)
+    y += 2*ROWSPACING
+    y, branches = tree([[{"Language": "English", "Word": "'God be with you!'"}, {"Language": "English", "Word": "'good'"}, {"Language": "English", "Word": "'good morning'"}],
+                [{"Language": "", "Word": "'goodbye'"}, {"Language": "", "Word": "'goodbye'"}, {"Language": "", "Word": "'goodbye'"}]], y, font)
+    
+    
     while running:
         # main loop
-        screen = def_screen(SWIDTH, SHEIGHT)
-        clock = pygame.time.Clock()
-
         clock.tick(120)
-        screen.fill(black)
+        
+        obj += branches
+        obj += words
 
         for el in obj:
             el.render(screen, view_pos)
+            
+        # events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                    screen_draging = True
+                    for el in obj:
+                        mouse_x, mouse_y = event.pos
+                        offset_x = mouse_x
+
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:            
+                    screen_draging = False
+    
+            elif event.type == pygame.MOUSEMOTION:
+                if screen_draging:
+                    mouse_x, mouse_y = event.pos
+                    for el in obj:
+                        view_pos = mouse_x + offset_x
+                        el.render(screen, view_pos)
 
         pygame.display.flip()
-
-        running = check_for_quit()
-
-
-class Box:
-    def __init__(self, color=white, pass_args=None, **kwargs):
-        self._color = color
-
-        if pass_args is None:
-            pass_args = kwargs
-        else:
-            pass_args.update(kwargs)
-        self.rect = create_rect(pass_args)
-
-    def render(self, screen: pygame.Surface, view_pos):
-        round_rect(screen, self.rect.move(view_pos), self._color)
-
-
-class BoxText:
-    def __init__(self, text: str, font: pygame.font.Font, border=15, bcolor=white, tcolor=black, **kwargs):
-        self._color = bcolor
-
-        self.text: Text = Text(text, font, tcolor, x=0, y=0)
-
-        kwargs['width'] = self.text.rect.width + border * 2
-        kwargs['height'] = self.text.rect.height + border * .5
-        self.box = Box(bcolor, kwargs)
-
-        self.text.rect.center = self.box.rect.move(0, -self.text.rect.height * .07).center
-
-    def get_height(self) -> int:
-        return self.box.rect.height
-
-    def render(self, screen: pygame.Surface, view_pos):
-        self.box.render(screen, view_pos)
-        self.text.render(screen, view_pos)
-
-
-class Text:
-    def __init__(self, text: str, font: pygame.font.Font, color=white, **kwargs):
-        self._surface: pygame.Surface = font.render(text, True, color)
-        self.rect = pygame.Rect(0, 0, self._surface.get_width(), self._surface.get_height())
-
-        if 'x' in kwargs.keys():
-            self.rect.x = kwargs['x']
-        if 'y' in kwargs.keys():
-            self.rect.y = kwargs['y']
-        if 'center_x' in kwargs.keys():
-            self.rect.centerx = kwargs['center_x']
-        if 'center_y' in kwargs.keys():
-            self.rect.centery = kwargs['center_y']
-
-    def render(self, screen: pygame.Surface, view_pos):
-        screen.blit(self._surface, self.rect.topleft + view_pos)
-
+        
+def def_word(word, font, yi):
+    words = []
+    # create object "Word:"
+    word_txt = BoxText("Word:", font, center_x=cx, y=yi)
+    words.append(word_txt)
+    # create object for the chosen word.
+    y = yi + word_txt.box.rect.height + ROWSPACING*(3/4)
+    word = BoxText(word, font, center_x=cx, y=y)
+    words.append(word)
+    
+    yf = y + word.box.rect.height
+    
+    return yf, words
 
 def tree(tree, starting_y, font):
     """
     The tree should be of the form
-    [[{"Language": "Latin", "Word": "sonus"}, {"Language": "", "Word": ""}],
-     [{"Language": "Anglo-Norman French", "Word": "soun/suner"}, {"Language": "", "Word": ""}],
-     [{"Language": "Middle English", "Word": "soun"},{"Language": "English", "Word": "-d"}],
-     [{"Language": "", "Word": "sound"}, {"Language": "", "Word": "sound"}]]
+    [[{"Language": "Latin", "Word": "'sonus'"}, {"Language": "", "Word": ""}],
+     [{"Language": "Anglo-Norman French", "Word": "'soun/suner'"}, {"Language": "", "Word": ""}],
+     [{"Language": "Middle English", "Word": "'soun'"},{"Language": "English", "Word": "'-d'"}],
+     [{"Language": "", "Word": "'sound'"}, {"Language": "", "Word": "'sound'"}]]
     with "" representing empty space. Location of empty and non-empty space and the same dictionary (combined branches)
     indicate the tree branches. The list goes from top to bottom. The last word should not have language written down.
-    Put repeating elements next to one another, in the columns where they step from.
+    Put repeating elements next to one another, in the columns where they step from. Please put the "Word"s in quotes for clarity.
     """
     # [[language y], [word y]]
     columns = len(tree[0])
     col_spacing = SWIDTH / (columns + 1)
     prevcols = None
-    y_tot = starting_y - (ARROWLENGTH + 2 * ROWSPACING)
     y_larrow = 0
     row = 0
     obj = []
     overlap = 2
+    
+    # create object "Origin Tree:"
+    y_tot = starting_y
+    origin_txt = BoxText("Origin Tree:", font, center_x=cx, y=y_tot)
+    obj.append(origin_txt)
+    
+    y_tot += origin_txt.box.rect.height + ROWSPACING - (ARROWLENGTH + 2 * ROWSPACING)
+    
     for row in tree:
         y_vals = [[0], [0], [0]]
         heights = [[0], [0], [0]]
+        
+        y_larrow = y_tot + ROWSPACING
+        
         y_tot += ARROWLENGTH + 2 * ROWSPACING
 
         elements = []
@@ -178,8 +174,6 @@ def tree(tree, starting_y, font):
                 y_vals[1].append(y_word)
                 heights[1].append(word.box.rect.height)
 
-                prevcols = 0
-
             if el["Language"] != "" and el["Word"] != "":
                 len_cline = abs(word.box.rect.centery - lang.box.rect.centery - lang.box.rect.height + overlap * 2)
                 cline = Box(center_x=0, y=y_cline, width=LINEWIDTH, height=len_cline)
@@ -210,85 +204,35 @@ def tree(tree, starting_y, font):
             if el["Language"] != "" and el["Word"] != "":
                 cline = Box(center_x=x_col, y=y_cline, width=LINEWIDTH, height=len_cline)
                 obj.append(cline)
-
-                y_larrow = y_tot + ROWSPACING
-                larrow = Box(center_x=x_col, y=y_larrow, width=LINEWIDTH, height=ARROWLENGTH)
-                obj.append(larrow)
+            
+        if prevcols != None:
+            x_col = 0
+            for el in row:
+                x_col += col_spacing
+                
+                row_i = tree.index(row)
+                el_i = row.index(el)
+                row_up = tree[row_i - 1]
+                el_up = row_up[el_i]
+                
+                print("el:", el)
+                print("row:", row)
+                print("el_up:", el_up)
+                print("row_up:", row_up)
+                
+                if el["Word"] != "" and el_up["Word"] != "":
+                    larrow = Box(center_x=x_col, y=y_larrow, width=LINEWIDTH, height=ARROWLENGTH)
+                    obj.append(larrow)
 
         prevcols = columns
-
-    return obj
-
-
-def create_rect(pass_args):
-    rect = pygame.Rect(0, 0, 0, 0)
-    if 'x' in pass_args.keys():
-        rect.x = pass_args['x']
-    if 'y' in pass_args.keys():
-        rect.y = pass_args['y']
-    if 'width' in pass_args.keys():
-        rect.width = pass_args['width']
-    if 'height' in pass_args.keys():
-        rect.height = pass_args['height']
-    if 'center_x' in pass_args.keys():
-        rect.x = pass_args['center_x'] - rect.width / 2
-    if 'center_y' in pass_args.keys():
-        rect.y = pass_args['center_y'] - rect.height / 2
-    return rect
-
+    
+    return y_tot, obj
 
 def branch_space(branches):
     return SWIDTH / (branches + 1)
 
-
-def check_for_quit():
-    boolean = True
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            boolean = False
-    return boolean
-
-
 def def_screen(width, height):
     return pygame.display.set_mode((width, height))
-
-
-def round_rect(surface, rect, color, radius=0.4):
-    """
-    surface : destination
-    rect    : rectangle
-    color   : rgb or rgba
-    radius  : 0 <= radius <= 1
-    """
-
-    rect = pygame.Rect(rect)
-    color = pygame.Color(*color)
-    alpha = color.a
-    color.a = 0
-    pos = rect.topleft
-    rect.topleft = 0, 0
-    rectangle = pygame.Surface(rect.size, pygame.SRCALPHA)
-
-    circle = pygame.Surface([min(rect.size) * 3] * 2, pygame.SRCALPHA)
-    pygame.draw.ellipse(circle, (0, 0, 0), circle.get_rect(), 0)
-    circle = pygame.transform.smoothscale(circle, [int(min(rect.size) * radius)] * 2)
-
-    radius = rectangle.blit(circle, (0, 0))
-    radius.bottomright = rect.bottomright
-    rectangle.blit(circle, radius)
-    radius.topright = rect.topright
-    rectangle.blit(circle, radius)
-    radius.bottomleft = rect.bottomleft
-    rectangle.blit(circle, radius)
-
-    rectangle.fill((0, 0, 0), rect.inflate(-radius.w, 0))
-    rectangle.fill((0, 0, 0), rect.inflate(0, -radius.h))
-
-    rectangle.fill(color, special_flags=pygame.BLEND_RGBA_MAX)
-    rectangle.fill((255, 255, 255, alpha), special_flags=pygame.BLEND_RGBA_MIN)
-
-    return surface.blit(rectangle, pos)
-
 
 if __name__ == '__main__':
     main()
